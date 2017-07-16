3434eed5a112e1755a4940efaab07310efed960f
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-07-16 14:19:58 -0300

    Creating display component

---
+++ src/components/Display.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default class Display extends React.Component {
+    render(){
+        return (
+            <div id="display">
+                <h1>0</h1>
+            </div>
+        )
+    }
+}
