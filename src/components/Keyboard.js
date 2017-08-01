9d3c7263d1920d002a8d1fb27c46d3fd71eac872
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-01 18:29:53 -0300

    Implementing the buttons values

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,30 +1,95 @@
 import React from 'react'
 import Button from './Button'
+
 export default class Keyboard extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
     render(){
         return(
             <div id="keyboard">
-                <Button valor="c"/>
-                <Button valor="+/-"/>
-                <Button valor="%"/>
-                <Button valor="/"/>
-                <Button valor="7"/>
-                <Button valor="8"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="c"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="+/-"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="%"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="/"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="7"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="8"/>
 
-                <Button valor="9"/>
-                <Button valor="*"/>
-                <Button valor="4"/>
-                <Button valor="5"/>
-                <Button valor="6"/>
-                <Button valor="+"/>
-                <Button valor="4"/>
-                <Button valor="5"/>
-                <Button valor="6"/>
-                <Button valor="-"/>
-                <Button valor="0"/>
-                <Button valor="."/>
-                <Button valor="pi"/>
-                <Button valor="="/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="9"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="*"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="4"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="5"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="6"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="+"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="4"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="5"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="6"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="-"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="0"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="."/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="pi"/>
+                <Button valueInMemory={this.props.vulueInMemory}
+                        displayValue={this.props.resultLastOperation}
+                        cleanDisplayNextOperation={this.props.cleanDisplayNextOperation}
+                        decimalMode={this.props.decimalMode} valor="="/>
             </div>
         )
     }
