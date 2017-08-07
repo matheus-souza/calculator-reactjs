8e9acdefa3a750d030a0e97b5c1829d1811ff778
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-07 18:35:14 -0300

    Refactoring Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import Display from './Display'
-import Keyboard from './Keyboard'
+import Visor from './Display'
+import Teclado from './Keyboard'
 
 import {calculatorStore} from './../flux/CalculatorStore'
 
-export default class Calculator extends React.Component {
-    constructor(props) {
+export default class Calculator extends React.Component{
+
+    constructor(props){
         super(props)
         this.state = calculatorStore.getState()
     }
 
     componentWillMount(){
-        calculatorStore.on('change', () => {
+        calculatorStore.on('change', ()=>{
             this.setState(calculatorStore.getState())
         })
     }
@@ -19,12 +20,9 @@ export default class Calculator extends React.Component {
     render(){
         return (
             <div id="calculator">
-                <Display valor = {this.state.displayValue}/>
-                <Keyboard valueInMemory={this.state.resultLastOperation}
-                          resultLastOperation={this.state.arithmeticOperation}
-                          displayValue={this.state.displayValue}
-                          cleanDisplayNextOperation={this.state.cleanDisplay}
-                          decimalMode={this.state.decimalMode} />
+                <Visor value={this.state.valueAtDisplay}/>
+                <Teclado valueInMemory={this.state.resultLastOperation} previousOperation={this.state.arithmeticOperation} displayValue={this.state.valueAtDisplay} cleanInNextOperation={this.state.cleanDisplay}
+                         decimalEntry={this.state.entryDecimalMode} />
             </div>
         )
     }
