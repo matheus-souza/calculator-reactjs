4845068d7591e46448b7947beb8c67a49d9bc662
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-07 18:34:35 -0300

    Refactoring Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as Action from './../flux/CalculatorActions'
 
-export default class Button extends React.Component {
+export default class Button extends React.Component{
     constructor(props){
         super(props)
     }
@@ -9,13 +9,13 @@ export default class Button extends React.Component {
     render(){
         return (
             <div className="button" onClick={this.click.bind(this)}>
-                <p>{this.props.valor}</p>
+                <p>{this.props.value}</p>
             </div>
         )
     }
 
     click(e){
-        switch (this.props.valor){
+        switch(this.props.value){
             case '0':
             case '1':
             case '2':
@@ -26,20 +26,22 @@ export default class Button extends React.Component {
             case '7':
             case '8':
             case '9':
-                Action.updateDisplay(this.props.displayValue, this.props.valor, this.props.CleanDisplayNextOperation, this.props.decimalMode)
+                Action.updateDisplay(this.props.displayValue,this.props.value
+                    ,this.props.cleanInNextOperation,this.props.decimalEntry)
                 break;
             case '+':
             case '-':
             case '*':
             case '/':
             case '=':
-                Action.executeMathematicsOperation(this.props.valor, this.props.resultLastOperation, this.propr.valueInMemory)
+                Action.executeMathematicaOperation(this.props.value,this.props.previousOperation
+                    ,this.props.displayValue,this.props.valueInMemory)
                 break;
             case '.':
-                Action.setDecimalMode(true)
+                Action.setEntryDecimalMode(true)
                 break;
             case 'c':
-                Action.reset();
+                Action.resetar();
                 break;
         }
     }
