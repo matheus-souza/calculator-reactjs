ae0034428988b4c18e5119a34d99f34bdb86dcd2
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-08-07 18:21:03 -0300

    Refactoring CalculatorActions.js

--- src/flux/CalculatorActions.js
+++ src/flux/CalculatorActions.js
@@ -1,88 +1,94 @@
 import {appDispatcher} from './AppDispatcher'
 import Action from './Constants'
 
-export function updateDisplay(oldValue,pressingValue,CleanDisplayNextOperation,decimalMode){
-    //por padrao colocamos o valor pressionado para ser o novo valor
+export function updateDisplay(oldValue,pressingValue,cleanDisplayNextOperation,entryDecimalMode){
+    //By default we put the value pressed to be the new value
     let newValue = pressingValue;
-    //se nao vamos limpar o visor entao precisamos modificar valor present
-    if(!CleanDisplayNextOperation){
+    //Modify present value
+    if(!cleanDisplayNextOperation){
         oldValue = parseFloat(oldValue);
         pressingValue = parseFloat(pressingValue);
-        //se o modo de entrada nao eh decimal multiplicamos o valor por 10 e somamos o novo
-        //valor para criar o novo valor
-        if(!decimalMode){
+        //We multiply the value by 10 and add the new
+        if(!entryDecimalMode){
             newValue = oldValue*10+pressingValue;
         }
         else{
-            //em caso de modo decimil esse codigo coloca as casas depois da virgula
-            newValue = oldValue + pressingValue/Math.pow(10,numberDecimalPlaces(oldValue)+1)
+            //Puts the houses after the comma
+            newValue = oldValue + pressingValue/Math.pow(10,quantityDecimalPlaces(oldValue)+1)
         }
     }
     else{
-        //se entramos aqui eh porque vamos limpar o visor e colocar um novo valor
-        //nesse caso setamos setCleanDisplayNextOperation para falso
-        //pois o visor jah foi limpo
+        //Clean display and set new value
         setCleanDisplayNextOperation(false)
     }
-    //finalmente despachamamos a acao para os stores via appDispatcher
-    appDispatcher.dispatch({type:Action.DISPLAY_UPDATE,value:newValue})
+    //Dispatching action for stores per appDispatcher
+    appDispatcher.dispatch({type:Action.UPDATE_DISPLAY,value:newValue})
 }
-export function executeMathematicsOperation(currentOperation,operationInMemory,valueInDisplay,valueInMemory){
-    debugger
-    let resultado = valueInDisplay;
-    //lemos os valores e convertemos eles para float
-    valueInDisplay = parseFloat(valueInDisplay)
+
+export function executeMathematicaOperation(currentOperation,operationInMemory,valueInVisor,valueInMemory){
+    let result = valueInVisor;
+    //Read value and converting for float
+    valueInVisor = parseFloat(valueInVisor)
     valueInMemory = parseFloat(valueInMemory)
     appDispatcher.dispatch({type:Action.CLEAN_OPERATION})
-    setDecimalMode(false);
-    //se nao escolhemos = como operacao, precisamos guardar a operacao
+    setEntryDecimalMode(false);
+
+    //If not selected = as operation, saves operation
     if(currentOperation!== '='){
         appDispatcher.dispatch({type:Action.SET_OPERATION,value:currentOperation})
     }
-    //se jah temos uma operacao na memoria isso significa q eh hora
-    //de executar ela
+
+    //If operation is saved in memory, execute operation
     if(operationInMemory){
         switch(operationInMemory){
             case '+':
-                resultado = valueInDisplay+valueInMemory;
+                result = valueInMemory+valueInVisor;
                 break;
             case '-':
-                resultado = valueInDisplay-valueInMemory;
+                result = valueInMemory-valueInVisor;
                 break;
             case '*':
-                resultado = valueInDisplay*valueInMemory;
+                result = valueInMemory*valueInVisor;
                 break;
             case '/':
-                resultado = valueInDisplay/valueInMemory;
+                result = valueInMemory/valueInVisor;
                 break;
         }
     }
-    //finalmente despachamos o novo valor
-    appDispatcher.dispatch({type:Action.SAVE_VALUE,value:resultado});
-    blink(resultado)
+
+    //Dispatching new value
+    appDispatcher.dispatch({type:Action.SAVE_VALUE,value:result});
+    blink(result)
     setCleanDisplayNextOperation(true)
+
 }
+
 export function setCleanDisplayNextOperation(value){
-    appDispatcher.dispatch({type:Action.CLEAN_DISPLAY_NEXT_OPERATION,value:value})
+    appDispatcher.dispatch({type:Action.CLEAN_DISPLAY_IN_NEXT_OPERATION,value:value})
 }
-export function setDecimalMode(value){
-    appDispatcher.dispatch({type:Action.DECIMAL_MODE,value:value})
+
+export function setEntryDecimalMode(value){
+    appDispatcher.dispatch({type:Action.ENTRY_WITH_DECIMAL_NUMBERS,value:value})
 }
-export function setDisplay(value){
-    appDispatcher.dispatch({type:Action.DISPLAY_UPDATE,value:value})
+
+export function setVisor(value){
+    appDispatcher.dispatch({type:Action.UPDATE_DISPLAY,value:value})
 }
-export function reset(){
+
+export function resetar(){
     appDispatcher.dispatch({type:Action.SAVE_VALUE,value:0});
     appDispatcher.dispatch({type:Action.CLEAN_OPERATION});
     blink(0)
 }
+
 export function blink(value){
-    setDisplay('')
+    setVisor('')
     setTimeout(() =>{
-        setDisplay(value)
+        setVisor(value)
     },100)
 }
-function numberDecimalPlaces(value) {
+
+function quantityDecimalPlaces(value) {
     if(Math.floor(value) === value) return 0;
     return value.toString().split(".")[1].length || 0;
 }
